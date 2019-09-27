f61973ae799946359f8e757c8d3c596e29542227
Miguel Jimenez <denkatalabeno@gmail.com>
2019-09-27 15:26:32 +0800

    can/t copy this shit

--- gdocs.rb
+++ gdocs.rb
@@ -1,19 +1,17 @@
+require 'pry'
 require 'bundler'
-
 Bundler.require
 
-# Authenticate a session with your Service Account
-session = GoogleDrive::Session.from_service_account_key("client_secrets.json")
 
-
-def unique_shuffle(array)
-  shuff = array.shuffle
-    if @combination_array.include?(shuff)
-      unique_shuffle(shuff)
-    else
-      @combination_array << shuff
-    end
+# Initialize the client with your Bearer API key
+# (add this to config/initializers/bearer.rb)
+Bearer::Configuration.setup do |config|
+  config.api_key = 'sk_sandbox_SIY13hyGui7kUjCqKd0HcZfQhnp9qAFB'
 end
+# Call an endpoint of your api
+# Bearer.integration('google_docs')
+#   .auth('ac727aa0-e0ef-11e9-9c0c-8ba5d28524e8')
+#   .post('documents', query: { title: "Bearer 🐻" })
 
 bs_bingo_words = %w(Test Driven Development Story Time  Epic  Waterfall Manifesto
 Done  Task Board  Unit Test Time-box  Backlog
@@ -22,26 +20,40 @@ Sprint Review Pigs  AGILE BINGO
 Burn-down Scrum User Story  Pattern Kaizen
 Story Point Estimation Refactor Regression test Stand-up)
 
+# Generate some random agile words - Done
+
 @combination_array = []
-participants_number = 20
 
-# Get the spreadsheet by its title
-@spreadsheet = session.spreadsheet_by_title("Bingo")
-# Get the first worksheet
-@worksheet = @spreadsheet.worksheets.first
-# Print out the first 6 columns of each row
-
-
-col = 1
-
-20.times do
-   unique_shuffle(bs_bingo_words)
-   uniq = @combination_array.last
-   uniq.each_with_index do |word, i|
-    @worksheet[i+1, col] = word
-   end
-   col += 1
+def unique_shuffle(array)
+  shuffled = array.shuffle
+    if @combination_array.include?(shuffled)
+      unique_shuffle(shuff)
+    else
+      @combination_array << shuff
+    end
 end
 
-@worksheet.save
+# Make a copy of a template in the folder
+## Create the template on my google docs - Done
+## Activate the Google Docs API - Done
+## Authenticate into Gdrive -
 
+gdrive_session = GoogleDrive::Session.from_service_account_key("client_secrets.json")
+## Fetch the document
+gdrive_session.files.each do |file|
+  p file.title
+end
+
+binding.pry
+
+puts "hello"
+
+## Make a copy and provide the file with a name
+
+
+## Authenticate into Google Docs -
+
+# Get that last document that was just generated
+# That document should have a table inside with the exact number of cells
+# for the words that we got
+# Iterate through the cells and add the document
